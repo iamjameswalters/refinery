9d140f47f60c8332c75c489e58fe87479c82b90f
James Walters <33947683+iamjameswalters@users.noreply.github.com>
2024-07-07 21:51:10 -0400

    Add home page URL and view

--- refinery_project/refinery_app/views.py
+++ refinery_project/refinery_app/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView, CreateView
+
+from . import models
 
 # Create your views here.
+
+class HomePage(TemplateView):
+    template_name = "refinery/home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        current_plan = models.Plan.objects.first()
+        if current_plan:
+            context["plan"] = current_plan
+        return context
+    
+
+# class NewPlan(CreateView):
+#     template_name = "refinery/new_plan.html"    
+#     model = models.Plan
+    
