aff9a0a131882bb80662aa3454cb2929cb6595c5
James Walters <33947683+iamjameswalters@users.noreply.github.com>
2024-08-11 23:59:20 -0400

    ruff format

--- refinery_project/refinery_app/forms.py
+++ refinery_project/refinery_app/forms.py
@@ -1,12 +1,16 @@
 from django import forms
 from . import models
 
+
 class PlanForm(forms.ModelForm):
     class Meta:
         model = models.Plan
         fields = ["translation", "api_key", "verses_per_day", "days_of_review"]
 
+
 class BookForm(forms.ModelForm):
     class Meta:
         model = models.Book
-        fields = ["title",]
+        fields = [
+            "title",
+        ]
